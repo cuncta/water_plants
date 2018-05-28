811b537252ba0cd0d9b0bfc9713f3ad7b13c3252
oldsimo <simonevadi88@hotmail.com>
2018-05-28 17:48:48 +0200

    Add the DS3231 clock module to arduino

--- README.md
+++ README.md
@@ -34,6 +34,12 @@ In this project Pizero W is used to make a website where you can set up set the 
   * relay pin to digital pin 7
   * the power supply of the submersible pump must be connected to this relay
 
+* DS3231 Clock
+  * GND to ground
+  * Vcc to 5V
+  * SCL to SCL in arduino
+  * SDA to SDA in arduino
+
 ### Wiring to the second arduino
 
 * Bluetooth Module:
@@ -66,7 +72,7 @@ In this project Pizero W is used to make a website where you can set up set the 
 
 There are three parts to this setup. Two files are the arduino sketches, and the other runs a local web server, and some files are used to store data.
 * Arduino
-    * *arduino_water_bt.ino* this is the arduino sketch. Arduino will check wether there is an incoming data from bluetooth. If it receives something there are four possibilities:
+    * *arduino_water_bt.ino* this is the arduino sketch. Arduino will check wether there is an incoming data from bluetooth. At the first setup uncomment line 102 (//setDS3231time(00,42,17,2,28,05,18);) and set the proper time. Then remember to comment it again if you plan to change the upload the code in arduino again. If it receives something there are four possibilities:
         * "w" it activates the pump for a certain amount of seconds, default is 8.
         * "h" it waits for the next command
         * "0-9" if it receives a number between 0 and 9 it changes the watering time to that value
--- arduino_water_bt.ino
+++ arduino_water_bt.ino
@@ -25,7 +25,7 @@ SoftwareSerial bluetooth(rxpin, txpin);
 // define the pin that control the relay
 #define RELAY1  7
 
-// initializing the OLED display
+//initializing the OLED display
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -64,18 +64,29 @@ B00000000, B00110000 };
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
+//Initializig DS3231 clock
+#include "Wire.h"
+#define DS3231_I2C_ADDRESS 0x68
+// Convert normal decimal numbers to binary coded decimal
+byte decToBcd(byte val)
+{
+return( (val/10*16) + (val%10) );
+}
+// Convert binary coded decimal to normal decimal numbers
+byte bcdToDec(byte val)
+{
+return( (val/16*10) + (val%16) );
+}
+
+
 void setup() {
 //INITIALIZE OLED SCREEN
 Serial.begin(9600);
 // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
 display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-
 display.display();
 delay(2000);
 display.clearDisplay();
-
-
-
 
 //INITIALIZE RELAY
 pinMode(RELAY1, OUTPUT);
@@ -85,11 +96,17 @@ digitalWrite(RELAY1,1);
 bluetooth.begin(9600);
 bluetooth.println("Bluetooth ready");
 
+//INITIALIZE CLOCK
+// set the initial time here:
+// DS3231 seconds, minutes, hours, day, date, month, year
+//setDS3231time(00,42,17,2,28,05,18);
 
 }
 
 void loop() {
 
+Serial.println("\n");
+ // display the real-time clock data on the Serial Monitor,
 
   //Check for new data
 if(bluetooth.available()){
@@ -103,42 +120,23 @@ if(bluetooth.available()){
 //water the plants if received data is w
 if( k == 'w' ){
     // text display tests
-    display.clearDisplay();
-    display.setTextSize(3);
-    display.setTextColor(WHITE);
-    display.setCursor(0,0);
-    display.println("Watering");
-    display.display();
+  displayTime("Water");
   water_plants(dur);
   k= 'h';
   // text display tests
-    display.clearDisplay();
-    display.setTextSize(3);
-    display.setTextColor(WHITE);
-    display.setCursor(0,0);
-    display.println("Waiting ");
-    display.display();
+    displayTime("Waiting");
 }
 //wait if received data is h
 else if( k == 'h') {
   // text display tests
     display.clearDisplay();
-    display.setTextSize(3);
-    display.setTextColor(WHITE);
-    display.setCursor(0,0);
-    display.println("Waiting");
-    display.display();
+   displayTime("Waiting");
 }
 //change watering duration
 else if (isDigit(k)) {
     dur = k - '0';
     // text display tests
-    display.clearDisplay();
-    display.setTextSize(2);
-    display. setTextColor(WHITE);
-    display.setCursor(0,0);
-    display.println("Watering duration:"+ String(dur));
-    display.display();
+displayTime("Dur.set");
   }
 else {
   //lcd.clear();
@@ -158,4 +156,99 @@ void water_plants(int dur){
 
  digitalWrite(RELAY1,1);          // Turns Relay Off
  delay(2000);
+}
+
+//Functions for the clock
+void setDS3231time(byte second, byte minute, byte hour, byte dayOfWeek, byte
+dayOfMonth, byte month, byte year)
+{
+// sets time and date data to DS3231
+Wire.beginTransmission(DS3231_I2C_ADDRESS);
+Wire.write(0); // set next input to start at the seconds register
+Wire.write(decToBcd(second)); // set seconds
+Wire.write(decToBcd(minute)); // set minutes
+Wire.write(decToBcd(hour)); // set hours
+Wire.write(decToBcd(dayOfWeek)); // set day of week (1=Sunday, 7=Saturday)
+Wire.write(decToBcd(dayOfMonth)); // set date (1 to 31)
+Wire.write(decToBcd(month)); // set month
+Wire.write(decToBcd(year)); // set year (0 to 99)
+Wire.endTransmission();
+}
+void readDS3231time(byte *second,
+byte *minute,
+byte *hour,
+byte *dayOfWeek,
+byte *dayOfMonth,
+byte *month,
+byte *year)
+{
+Wire.beginTransmission(DS3231_I2C_ADDRESS);
+Wire.write(0); // set DS3231 register pointer to 00h
+Wire.endTransmission();
+Wire.requestFrom(DS3231_I2C_ADDRESS, 7);
+// request seven bytes of data from DS3231 starting from register 00h
+*second = bcdToDec(Wire.read() & 0x7f);
+*minute = bcdToDec(Wire.read());
+*hour = bcdToDec(Wire.read() & 0x3f);
+*dayOfWeek = bcdToDec(Wire.read());
+*dayOfMonth = bcdToDec(Wire.read());
+*month = bcdToDec(Wire.read());
+*year = bcdToDec(Wire.read());
+}
+void displayTime(String mystr)
+{
+byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+// retrieve data from DS3231
+readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month,
+&year);
+
+display.clearDisplay();
+display.setTextSize(3);
+display.setTextColor(WHITE);
+display.setCursor(0,40);
+display.print(mystr);
+display.setTextSize(2);
+display.setCursor(0,00);
+display.print(hour, DEC);
+// convert the byte variable to a decimal number when displayed
+display.print(":");
+if (minute<10)
+{
+  display.print("0");
+}
+display.print(minute, DEC);
+display.print(" ");
+switch(dayOfWeek){
+case 1:
+  display.print("Sun");
+  break;
+case 2:
+  display.print("Mon");
+  break;
+case 3:
+  display.print("Tue");
+  break;
+case 4:
+  display.print("Wed");
+  break;
+case 5:
+  display.print("Thu");
+  break;
+case 6:
+  display.print("Fr");
+  break;
+case 7:
+  display.print("Sat");
+  break;
+}
+display.setCursor(0,20);
+display.print(dayOfMonth, DEC);
+display.print("/");
+display.print(month, DEC);
+display.print("/");
+display.print(year, DEC);
+display.display();
+
+
+
 }
