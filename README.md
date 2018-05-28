19ad4300649b88352c9cbf957ac928e3da9d2982
oldsimo <simonevadi88@hotmail.com>
2018-05-28 15:18:39 +0200

    change from LCD to OLED display

--- README.md
+++ README.md
@@ -11,21 +11,20 @@ In this project Pizero W is used to make a website where you can set up set the 
 
 * Arduino Uno x2
 * Bluetooth module H5-05 x2
-* 16x2 LCD display (compatible with the Hitachi HD44780 driver)
+* 128x64 OLED display
 * Relay module
 * Submersible Pump
 * Soil moisture sensors x5
 
 ### Wiring to the first arduino
 
-* LCD screen:
-  * LCD RS pin to digital pin 12
-  * LCD Enable pin to digital pin 11
-  * LCD D4 pin to digital pin 5
-  * LCD D5 pin to digital pin 4
-  * LCD D6 pin to digital pin 3
-  * LCD D7 pin to digital pin 2
-  * Additionally, wire a 10k pot to +5V and GND, with it's wiper (output) to LCD screens VO pin (pin3). A 220 ohm resistor is used to power the backlight of the display, usually on pin 15 and 16 of the LCD connector
+* OLED screen:
+  * OLED GND pin to ground
+  * OLED VCC to 5V
+  * OLED SCL pin to A5
+  * OLED SDA pin to A4
+  * Check this nice To learn how to setup an OLED display and install useful libraries: [OLED tutorial](http://www.instructables.com/id/Monochrome-096-i2c-OLED-display-with-arduino-SSD13/ )
+
 
 * Bluetooth Module:
   * bluetooth rx pin to digital pin 8
--- arduino_water_bt.ino
+++ arduino_water_bt.ino
@@ -1,12 +1,8 @@
-  /*
-  
- */
+/*
 
-// include the library code:
-#include <LiquidCrystal.h>
+*/
 
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 
 //Include the module so we don't
 //have to use the default Serial
@@ -27,78 +23,139 @@ int dur = 8;
 SoftwareSerial bluetooth(rxpin, txpin);
 
 // define the pin that control the relay
-#define RELAY1  7                        
+#define RELAY1  7
 
+// initializing the OLED display
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+B00000001, B11000000,
+B00000001, B11000000,
+B00000011, B11100000,
+B11110011, B11100000,
+B11111110, B11111000,
+B01111110, B11111111,
+B00110011, B10011111,
+B00011111, B11111100,
+B00001101, B01110000,
+B00011011, B10100000,
+B00111111, B11100000,
+B00111111, B11110000,
+B01111100, B11110000,
+B01110000, B01110000,
+B00000000, B00110000 };
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
 
 void setup() {
-  //INITIALIZE LCD SCREEN
-  // set up the number of columns and rows on the LCD
-  lcd.begin(16, 2);
-  // set the cursor to column 0, line 1
-  // line 1 is the second row, since counting begins with 0
-  lcd.setCursor(0, 1);
-  // print to the second line
-  //lcd.print("Dio Porco!");
+//INITIALIZE OLED SCREEN
+Serial.begin(9600);
+// by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
 
-  //INITIALIZE RELAY 
-  pinMode(RELAY1, OUTPUT); 
-  digitalWrite(RELAY1,1);
+display.display();
+delay(2000);
+display.clearDisplay();
 
-  //INITIALIZE BLUETOOTH
-  bluetooth.begin(9600);
-  bluetooth.println("Bluetooth ready");
 
-  
+
+
+//INITIALIZE RELAY
+pinMode(RELAY1, OUTPUT);
+digitalWrite(RELAY1,1);
+
+//INITIALIZE BLUETOOTH
+bluetooth.begin(9600);
+bluetooth.println("Bluetooth ready");
+
+
 }
 
 void loop() {
-    
-    //Check for new data
-  if(bluetooth.available()){
-    //Remember new data
-    k = bluetooth.read();
-    //Print the data for debugging purposes
-    lcd.clear();
-    //lcd.print(k);
-  }
-  
-  //water the plants if received data is w
-  if( k == 'w' ){
-    lcd.clear();
-    lcd.print("watering");    
-    water_plants(dur);
-    k= 'h';
-    lcd.clear();
-    lcd.print("waiting");
-  }
-  //wait if received data is h
-  else if( k == 'h') {
-    lcd.clear();
-    lcd.print("waiting");
-  }
-  //change watering duration
-  else if (isDigit(k)) {
-      dur = k - '0';
-      lcd.clear();
-      lcd.print(dur);
-    }
-  else {
-    lcd.clear();
-    lcd.print(k);
 
+
+  //Check for new data
+if(bluetooth.available()){
+  //Remember new data
+  k = bluetooth.read();
+  //Print the data for debugging purposes
+  //lcd.clear();
+  //lcd.print(k);
+}
+
+//water the plants if received data is w
+if( k == 'w' ){
+    // text display tests
+    display.clearDisplay();
+    display.setTextSize(3);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.println("Watering");
+    display.display();
+  water_plants(dur);
+  k= 'h';
+  // text display tests
+    display.clearDisplay();
+    display.setTextSize(3);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.println("Waiting ");
+    display.display();
+}
+//wait if received data is h
+else if( k == 'h') {
+  // text display tests
+    display.clearDisplay();
+    display.setTextSize(3);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.println("Waiting");
+    display.display();
+}
+//change watering duration
+else if (isDigit(k)) {
+    dur = k - '0';
+    // text display tests
+    display.clearDisplay();
+    display.setTextSize(2);
+    display. setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.println("Watering duration:"+ String(dur));
+    display.display();
   }
-  //Wait 100 milliseconds to decrease unnecessary hardware strain
-   delay(100);
-  
+else {
+  //lcd.clear();
+  ///lcd.print(k);
+
+}
+//Wait 100 milliseconds to decrease unnecessary hardware strain
+ delay(100);
+
 }
 
 // define watering function that waters for "dur" seconds
 // how long to water is set by variable "dur", default is 8 seconds
 void water_plants(int dur){
-   digitalWrite(RELAY1,0);          // Turns ON Relays 1
-   delay(dur*1000);                     // Wait 2 seconds
+ digitalWrite(RELAY1,0);          // Turns ON Relays 1
+ delay(dur*1000);                     // Wait 2 seconds
 
-   digitalWrite(RELAY1,1);          // Turns Relay Off
-   delay(2000);
+ digitalWrite(RELAY1,1);          // Turns Relay Off
+ delay(2000);
 }
-
