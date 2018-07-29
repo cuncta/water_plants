703f206c5db3c165d52327d6cfa7a41149511d6b
oldsimo <simonevadi88@hotmail.com>
2018-07-29 09:10:19 +0200

    added log book

--- water.py
+++ water.py
@@ -11,108 +11,120 @@ port = 1
 
 init = False
 
-def tail( f, lines=20 ):
-    total_lines_wanted = lines
 
-    BLOCK_SIZE = 1024
-    f.seek(0, 2)
-    block_end_byte = f.tell()
-    lines_to_go = total_lines_wanted
-    block_number = -1
-    blocks = [] # blocks of size BLOCK_SIZE, in reverse order starting
-                # from the end of the file
-    while lines_to_go > 0 and block_end_byte > 0:
-        if (block_end_byte - BLOCK_SIZE > 0):
-            # read the last block we haven't yet read
-            f.seek(block_number*BLOCK_SIZE, 2)
-            blocks.append(f.read(BLOCK_SIZE))
-        else:
-            # file too small, start from begining
-            f.seek(0,0)
-            # only read what was not read
-            blocks.append(f.read(block_end_byte))
-        lines_found = blocks[-1].count('\n')
-        lines_to_go -= lines_found
-        block_end_byte -= BLOCK_SIZE
-        block_number -= 1
-    all_read_text = ''.join(reversed(blocks))
-    return '\n'.join(all_read_text.splitlines()[-total_lines_wanted:])
 def get_last_watered():
 	try:
 		with open("last_watered.txt", "r") as f:
-		    return tail(f,1)
+			return tail(f,1)
 	except:
 		return "NEVER!"
 def get_next_water():
+	update_logbook("checked next water")
 	try:
 		with open("next_water.txt", "r") as f:
-		    return tail(f,1)
+			return tail(f,1)
 	except:
 		return "NEVER!"
-
 def get_watering_duration():
-    with open('watering_parameters.txt') as handle:
-    	wp = json.loads(handle.read())
-    m = "The watering duration is set to "+str(wp["watering_time"])+" seconds"
-    return m
+	update_logbook("check watering duration")
+	with open('watering_parameters.txt') as handle:
+		wp = json.loads(handle.read())
+	m = "The watering duration is set to "+str(wp["watering_time"])+" seconds"
+	return m
 def get_soil_status():
-    m = receive_soil_sensor_data()
-    m = ast.literal_eval(m)
-    text = "Soil status in format plant:value \n\r          W1:"+str(m["S0"])+"   W2:"+str(m["S1"])+"   W3:"+str(m["S2"])+"   C1:"+str(m["S3"])+"   C2:"+str(m["S4"])
-    return text
-def find(s, ch):
-    return [i for i, ltr in enumerate(s) if ltr == ch]
+	update_logbook("get soil status")
+	m = receive_soil_sensor_data()
+	m = ast.literal_eval(m)
+	text = "Soil status in format plant:value \n\r          W1:"+str(m["S0"])+"   W2:"+str(m["S1"])+"   W3:"+str(m["S2"])+"   C1:"+str(m["S3"])+"   C2:"+str(m["S4"])
+	return text
 def receive_soil_sensor_data():
-        bd_addr = "98:D3:91:F9:65:49"
-        port = 1
-        sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-        sock.connect((bd_addr, port))
-        print 'Connected to Elegoo'
-        message=[]
-        count = 0
-        while (count < 1000):
-                data = sock.recv(1)
-                message.append(data)
-                count += 1
-        sock.close
-        message =''.join(message)
+		bd_addr = "98:D3:91:F9:65:49"
+		port = 1
+		sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+		sock.connect((bd_addr, port))
+		print 'Connected to Elegoo'
+		message=[]
+		count = 0
+		while (count < 1000):
+				data = sock.recv(1)
+				message.append(data)
+				count += 1
+		sock.close
+		message =''.join(message)
 #        print 'message', message
-        n = find(message, "#")
-        return message[n[0]+1:n[1]]
+		n = find(message, "#")
+		update_logbook("received soil sensor data")
+		return message[n[0]+1:n[1]]
 def auto_water():
-    with open('watering_parameters.txt') as handle:
-    	wp = json.loads(handle.read())
-    with open("next_water.txt", "w+") as f1:
-       f1.write("Watering twice a day at "+str(wp["first_water"])+":00 and at "+str(wp["second_water"])+":00 for "+str(wp["watering_time"])+" seconds")
-    global scheduler
-    scheduler = BlockingScheduler()
-    scheduler.add_job(pump_on, 'cron',  day_of_week='mon-sun', hour=wp["first_water"], id='first_water')
-    scheduler.add_job(pump_on, 'cron',  day_of_week='mon-sun', hour=wp["second_water"], id='second_water')
-    scheduler.print_jobs()
-    scheduler.start()
-    return
+	with open('watering_parameters.txt') as handle:
+		wp = json.loads(handle.read())
+	with open("next_water.txt", "w+") as f1:
+	   f1.write("Watering twice a day at "+str(wp["first_water"])+":00 and at "+str(wp["second_water"])+":00 for "+str(wp["watering_time"])+" seconds")
+	update_logbook("Started auto watering")
+	global scheduler
+	scheduler = BlockingScheduler()
+	scheduler.add_job(pump_on, 'cron',  day_of_week='mon-sun', hour=wp["first_water"], id='first_water')
+	scheduler.add_job(pump_on, 'cron',  day_of_week='mon-sun', hour=wp["second_water"], id='second_water')
+	scheduler.print_jobs()
+	scheduler.start()
+	return
 def auto_water_off():
-    scheduler.remove_job('first_water')
-    scheduler.remove_job('second_water')
-    scheduler.print_jobs()
+	update_logbook("Stopped auto watering")
+	scheduler.remove_job('first_water')
+	scheduler.remove_job('second_water')
+	scheduler.print_jobs()
 def pump_on():
-    print 'watering'
-    with open("last_watered.txt", "a+") as f:
-        f.write("Last watered {:%Y-%m-%d %H:%M}\n".format(datetime.datetime.now()))
-    sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-    sock.connect((bd_addr, port))
-    sock.send('w')
-    time.sleep(10)
-    sock.send('h')
-    sock.close()
+	print 'watering'
+	with open("last_watered.txt", "a+") as f:
+		f.write("Last watered {:%Y-%m-%d %H:%M}\n".format(datetime.datetime.now()))
+	update_logbook("Pump on -- watering")
+	sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+	sock.connect((bd_addr, port))
+	sock.send('w')
+	time.sleep(10)
+	sock.send('h')
+	sock.close()
 def set_watering_time():
-    with open('watering_parameters.txt') as handle:
-        wp = json.loads(handle.read())
-    dur = wp["watering_time"]
-    print 'watering duration ',dur, 's'
-    sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-    sock.connect((bd_addr, port))
-    sock.send(dur)
-    time.sleep(10)
-    sock.send('h')
-    sock.close()
+	with open('watering_parameters.txt') as handle:
+		wp = json.loads(handle.read())
+	dur = wp["watering_time"]
+	message = "watering time set to" + dur + "seconds"
+	update_logbook(message)
+	print 'watering duration ',dur, 's'
+	sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+	sock.connect((bd_addr, port))
+	sock.send(dur)
+	time.sleep(10)
+	sock.send('h')
+	sock.close()
+def find(s, ch):
+	return [i for i, ltr in enumerate(s) if ltr == ch]
+def tail( f, lines=20 ):
+	total_lines_wanted = lines
+
+	BLOCK_SIZE = 1024
+	f.seek(0, 2)
+	block_end_byte = f.tell()
+	lines_to_go = total_lines_wanted
+	block_number = -1
+	blocks = [] # blocks of size BLOCK_SIZE, in reverse order starting
+				# from the end of the file
+	while lines_to_go > 0 and block_end_byte > 0:
+		if (block_end_byte - BLOCK_SIZE > 0):
+			# read the last block we haven't yet read
+			f.seek(block_number*BLOCK_SIZE, 2)
+			blocks.append(f.read(BLOCK_SIZE))
+		else:
+			# file too small, start from begining
+			f.seek(0,0)
+			# only read what was not read
+			blocks.append(f.read(block_end_byte))
+		lines_found = blocks[-1].count('\n')
+		lines_to_go -= lines_found
+		block_end_byte -= BLOCK_SIZE
+		block_number -= 1
+	all_read_text = ''.join(reversed(blocks))
+	return '\n'.join(all_read_text.splitlines()[-total_lines_wanted:])
+def update_logbook(message):
+	with open("log_Book.txt", "a+") as f:
+		f.write("{:%Y-%m-%d %H:%M} ".format(datetime.datetime.now()) + message + "\n")
